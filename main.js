e9a1b9f34854cb5dc0a3d1b7050c93ba446978e0
S M Tariqul Islam <79934984+smtariqulislam@users.noreply.github.com>
2024-03-02 17:17:51 +0600

    js file create in dom.

---
+++ main.js
@@ -0,0 +1,22 @@
+// Dom Manipulation
+
+// GetElementById()
+
+const title = document.getElementById('main-heading');
+
+// GetElementByClassName()
+
+const listItem= document.getElementsByClassName('list-items');
+
+// getElementsByTagName()
+
+const listems = document.getElementsByTagName('li');
+
+// querySelector()
+const container= document.querySelector('div);
+
+
+// querySelectorAll()
+
+const containers = document.querySelectorAll('div);
+//console.log()
